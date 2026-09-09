36c5fa18756ad4d6ea44fd39a3d239e7318efb65
junanhuang583-droid <junanhuang583@gmail.com>
2026-09-10 04:12:25 +0800

    chore: add public core exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./model/cards.js";
+export * from "./model/state.js";
+export * from "./core/rules-core-v1.js";
+export * from "./core/rule-helpers.js";
+export * from "./data/parse-card-record.js";
